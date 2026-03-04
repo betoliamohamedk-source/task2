9f76a1478b930a2cd3d042b31234f6043d3a7d90
Betolia <your_real_email@gmail.com>
2026-03-04 02:00:46 +0200

    task2

---
+++ lib/task2.dart
@@ -0,0 +1,54 @@
+// import 'dart:ffi';
+
+// // for loop
+// class PrintYear {
+//   PrintYear() {
+//     for (int i = 2006; i <= 2020; i++) {
+//       print("$i");
+//     }
+//   }
+// }
+
+// //  for loop
+// class PrintEvenNum {
+//   PrintEvenNum() {
+//     for (int i = 2; i <= 20; i += 2) {
+//       print(i);
+//     }
+//   }
+// }
+
+// //  nested loop
+// class PrintStar {
+//   PrintStar() {
+//     for (int i = 1; i <= 5; i++) {
+//       for (int j = 1; j <= i; j++) {
+//         print("*");
+//       }
+//       print("**");
+//     }
+//   }
+// }
+
+// // for loop
+// class SumEvenNum {
+//   SumEvenNum() {
+//     int sum = 0;
+//     for (int i = 2; i <= 10; i += 2) {
+//       sum += i;
+//       print("$sum");
+//     }
+//   }
+// }
+
+// // while loop
+// class PrintNames {
+//   PrintNames() {
+//     List names = ["tolina", "Menna", "Farida", "Reem"];
+//     int i = 0;
+//     while (i < names.length) {
+//       print(names[i]);
+//       i++;
+//     }
+//   }
+// }
---
+++ lib/tempCodeRunnerFile.dart
@@ -0,0 +1,7 @@
+class PrintYear {
+  PrintYear() {
+    for (int i = 2006; i <= 2020; i++) {
+      print("$i");
+    }
+  }
+}
